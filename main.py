61cfa568bb5a38420b2e3e81d7d74fd69a488f09
Kevin Jung <kev.jung.ii@gmail.com>
2021-11-15 21:53:33 -0600

    adds path for shopping list index

--- main.py
+++ main.py
@@ -10,3 +10,7 @@ async def root():
 @roomate_api.get("/recipes")
 async def recipies_index():
     return {"recipes": []}
+
+@roomate_api.get("/shopping_list")
+async def shopping_list_index():
+    return {"shopping_list": []}
